dbaa55661f938ef712cb369050dc5566a3952f43
ElzaFikriDinata <elzadinata360@gmail.com>
2026-09-22 00:37:26 +0700

    Connect Laravel to Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Vercel PHP berhasil!";
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
