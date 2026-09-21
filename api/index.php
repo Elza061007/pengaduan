a30939608526ab72b78b8763faab86a9d05496ed
ElzaFikriDinata <elzadinata360@gmail.com>
2026-09-22 00:51:01 +0700

    Use Laravel public entry point

--- api/index.php
+++ api/index.php
@@ -1,15 +1,3 @@
 <?php
 
-echo "1. PHP MASUK<br>";
-
-require __DIR__.'/../vendor/autoload.php';
-
-echo "2. COMPOSER BERHASIL<br>";
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-echo "3. LARAVEL BERHASIL BOOTSTRAP<br>";
-
-echo "4. COBA LOAD VIEW<br>";
-
-echo view('welcome');
+require __DIR__ . '/../public/index.php';
