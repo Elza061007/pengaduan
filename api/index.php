aaeb08446c4dcff47b8b37a3e8dcbcb4caec02bf
ElzaFikriDinata <elzadinata360@gmail.com>
2026-09-22 00:47:21 +0700

    Test Laravel welcome view

--- api/index.php
+++ api/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 echo "1. PHP MASUK<br>";
 
 require __DIR__.'/../vendor/autoload.php';
@@ -12,10 +10,6 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 echo "3. LARAVEL BERHASIL BOOTSTRAP<br>";
 
-$request = Request::capture();
+echo "4. COBA LOAD VIEW<br>";
 
-echo "4. REQUEST BERHASIL DIBUAT<br>";
-
-$response = $app->handleRequest($request);
-
-echo "5. HANDLE REQUEST SELESAI<br>";
+echo view('welcome');
